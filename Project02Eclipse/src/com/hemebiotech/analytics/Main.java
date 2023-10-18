2644cacb2e2e0e1206699430ebf611287440e5a4
Loic <loutix@hotmail.com>
2023-10-18 18:09:55 +0200

    Change main

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -1,12 +1,24 @@
 package com.hemebiotech.analytics;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    final static String SYMPTOMS_FILE_PATH = ".\\Project02Eclipse\\src\\resources\\symptoms.txt";
-    final static String DIRECTORY_PATH = ".\\Project02Eclipse\\src\\writer";
+    final static String SYMPTOMS_FILE_PATH = ".\\src\\resources\\symptoms.txt";
+    final static String DIRECTORY_PATH = ".\\src\\writer";
+
 
     public static void main(String[] args) throws IOException {
-        new AnalyticsCounter(SYMPTOMS_FILE_PATH, DIRECTORY_PATH);
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter(SYMPTOMS_FILE_PATH, DIRECTORY_PATH);
+
+        // add symptoms in ArrayList
+        final List<String> symptomsList = analyticsCounter.getSymptoms();
+
+        // count symptoms
+        final Map<String, Integer> symptomsCounted = analyticsCounter.countSymptoms(symptomsList);
+
+        // write out file
+        analyticsCounter.writeSymptoms(symptomsCounted);
     }
 }
