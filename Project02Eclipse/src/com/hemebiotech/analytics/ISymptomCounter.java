eaa6c39a1a8aa0876056ecb3b0279506d3e58f34
Loic <loutix@hotmail.com>
2023-10-19 11:52:53 +0200

    Refactor methods

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomCounter.java
@@ -4,12 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface ISymptomCounter {
-    /**
-     * Count occurrences of each existing symptom
-     *
-     * @param symptoms<String> symptoms
-     * @return symptomsOccurrence
-     */
+
     Map<String, Integer> countSymptoms(List<String> symptoms);
 }
 
--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -3,17 +3,7 @@ package com.hemebiotech.analytics;
 import java.io.IOException;
 import java.util.Map;
 
-/**
- * Anything that will write symptom data from a Map<String, Integer>
- * <p>
- * The implementation does not need to order the list
- */
 
 public interface ISymptomWriter {
-    /**
-     * write the contains of the map on out file
-     *
-     * @param symptoms<> symptom
-     */
     void writeSymptoms(Map<String, Integer> symptoms) throws IOException;
 }
